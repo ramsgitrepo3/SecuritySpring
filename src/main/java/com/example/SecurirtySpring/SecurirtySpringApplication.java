a5b4e52f43fa4eb077c01564b6d2047d21267700
kumarmaran <ramkumarselvam03@gmail.com>
2025-09-13 07:22:33 +0530

    first commit adding SpringSecurity dependancy in our application, user name user password it's provide console

---
+++ src/main/java/com/example/SecurirtySpring/SecurirtySpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecurirtySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurirtySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurirtySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SecurirtySpring/SecurirtySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecurirtySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurirtySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
